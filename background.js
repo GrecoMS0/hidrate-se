463ea0b26867496535a4e82cb6512c36523823aa
GrecoMS0 <mat066397@gmail.com>
2025-01-24 17:15:44 -0300

    Nova Funcionalidade e Alterações Style
    - Criada nova funcionalidade de desativação de notificações. Agora é possível escolher o tempo em que as notificações ficarão desativadas, sendo ativadas novamente automaticamente.
    - Melhorias nos estilos. Criadas linhas separadoras horizontais. Implementadas cores para o Status dos lembretes

--- background.js
+++ background.js
@@ -1,124 +1,139 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "setAlarm") {
         const interval = parseInt(request.interval);
+        const disableDuration = parseInt(request.disableDuration);
+        
         if (!isNaN(interval)) {
             chrome.alarms.create("drinkWaterReminder", { periodInMinutes: interval });
             console.log(`Alarme Definido para cada ${interval} minutos.`);
         } else {
             console.error("Intervalo Inválido!");
         }
+
+        if (!isNaN(disableDuration)) {
+            chrome.storage.sync.set({ reminderActive: false }, () => {
+                console.log(`Lembretes serão reativados após ${disableDuration} minutos.`);
+                chrome.alarms.create("reactivateReminder", { delayInMinutes: disableDuration });
+            });
+        }
     }
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-    chrome.storage.sync.get(["reminderActive", "reminderPosition", "playSound", "volume"], (data) => {
-        if (data.reminderActive) {
-            const position = data.reminderPosition || "top-right";
-            const messages = [
-                "💧 Hora de se hidratar!",
-                "Já bebeu sua água hoje? 💧",
-                "Bora ingerir um pouco de 💧H2O!!!",
-                "Não se esqueça de beber água! 💧",
-                "Seu corpo agradece um gole de água! 💧",
-                "Pausa para a hidratação! 💧",
-                "Água é vida! Beba um pouco agora! 💧",
-                "Mantenha-se hidratado(a)! 💧",
-                "Vamos beber água para manter a saúde! 💧",
-                "Hidratação já! 💧",
-                "Olha a pedra no rim em... pega sua água lá! 💧",
-                "Você é feito de 70% de água, então hidrate essa porcentagem! 💧",
-                "Sabe o que falta na sua vida? Água! Bebe aí! 💧",
-                "Mantenha-se hidratado, assim você evita de virar uma uva passa! 🍇💧",
-                "Beber água evita paulada... VAI SE HIDRATAR! 💧",
-                "Beber água deixa a pele bonita e os rins felizes! 💧😄",
-                "Quer evitar a ressaca? Bebe água agora! 💧",
-                "Não deixe sua garganta virar um deserto do Saara! 💧"
-            ];
-            const message = messages[Math.floor(Math.random() * messages.length)];
-
-            const audioFiles = [
-                "drop-01.mp3",
-                "drop-02.mp3",
-                "drop-03.mp3",
-                "drop-04.mp3"
-            ];
-
-            const audioFile = audioFiles[Math.floor(Math.random() * audioFiles.length)];
-
-            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-                chrome.scripting.executeScript({
-                    target: { tabId: tabs[0].id },
-                    func: (pos, msg, audio, playSound, volume) => {
-                        const reminderDiv = document.createElement('div');
-                        reminderDiv.textContent = msg;
-                        reminderDiv.style.position = "fixed";
-                        reminderDiv.style.backgroundColor = "rgba(0, 123, 255, 0.9)";
-                        reminderDiv.style.color = "white";
-                        reminderDiv.style.padding = "15px 25px";
-                        reminderDiv.style.borderRadius = "10px";
-                        reminderDiv.style.fontSize = "20px";
-                        reminderDiv.style.fontWeight = "bold";
-                        reminderDiv.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)";
-                        reminderDiv.style.zIndex = "9999";
-                        reminderDiv.style.display = "flex";
-                        reminderDiv.style.justifyContent = "center";
-                        reminderDiv.style.alignItems = "center";
-                        reminderDiv.style.transition = "opacity 0.5s";
-
-                        const closeButton = document.createElement('button');
-                        closeButton.textContent = "X";
-                        closeButton.style.marginLeft = "10px";
-                        closeButton.style.backgroundColor = "red";
-                        closeButton.style.color = "white";
-                        closeButton.style.border = "none";
-                        closeButton.style.borderRadius = "50%";
-                        closeButton.style.width = "25px";
-                        closeButton.style.height = "25px";
-                        closeButton.style.cursor = "pointer";
-                        closeButton.style.fontSize = "16px";
-                        closeButton.addEventListener('click', () => reminderDiv.remove());
-                        reminderDiv.appendChild(closeButton);
-
-                        switch (pos) {
-                            case "top-left":
-                                reminderDiv.style.top = "20px";
-                                reminderDiv.style.left = "20px";
-                                break;
-                            case "top-right":
-                                reminderDiv.style.top = "20px";
-                                reminderDiv.style.right = "20px";
-                                break;
-                            case "bottom-left":
-                                reminderDiv.style.bottom = "20px";
-                                reminderDiv.style.left = "20px";
-                                break;
-                            case "bottom-right":
-                                reminderDiv.style.bottom = "20px";
-                                reminderDiv.style.right = "20px";
-                                break;
-                            case "center":
-                                reminderDiv.style.top = "50%";
-                                reminderDiv.style.left = "50%";
-                                reminderDiv.style.transform = "translate(-50%, -50%)";
-                                break;
-                        }
-
-                        if (playSound) {
-                            const audioElement = new Audio(audio);
-                            audioElement.volume = volume / 100;
-                            audioElement.play();
-                        }
-
-                        document.body.appendChild(reminderDiv);
-                        reminderDiv.style.opacity = "1";
-                        setTimeout(() => {
-                            reminderDiv.style.opacity = "0";
-                            setTimeout(() => { reminderDiv.remove(); }, 500);
-                        }, 40000);
-                    },
-                    args: [position, message, chrome.runtime.getURL(audioFile), data.playSound, data.volume]
+    if (alarm.name === "reactivateReminder") {
+        chrome.storage.sync.set({ reminderActive: true }, () => {
+            console.log("Lembretes reativados automaticamente");
+        });
+    } else if (alarm.name === "drinkWaterReminder") {
+        chrome.storage.sync.get(["reminderActive", "reminderPosition", "playSound", "volume"], (data) => {
+            if (data.reminderActive) {
+                const position = data.reminderPosition || "top-right";
+                const messages = [
+                    "💧 Hora de se hidratar!",
+                    "Já bebeu sua água hoje? 💧",
+                    "Bora ingerir um pouco de 💧H2O!!!",
+                    "Não se esqueça de beber água! 💧",
+                    "Seu corpo agradece um gole de água! 💧",
+                    "Pausa para a hidratação! 💧",
+                    "Água é vida! Beba um pouco agora! 💧",
+                    "Mantenha-se hidratado(a)! 💧",
+                    "Vamos beber água para manter a saúde! 💧",
+                    "Hidratação já! 💧",
+                    "Olha a pedra no rim em... pega sua água lá! 💧",
+                    "Você é feito de 70% de água, então hidrate essa porcentagem! 💧",
+                    "Sabe o que falta na sua vida? Água! Bebe aí! 💧",
+                    "Mantenha-se hidratado, assim você evita de virar uma uva passa! 🍇💧",
+                    "Beber água evita paulada... VAI SE HIDRATAR! 💧",
+                    "Beber água deixa a pele bonita e os rins felizes! 💧😄",
+                    "Quer evitar a ressaca? Bebe água agora! 💧",
+                    "Não deixe sua garganta virar um deserto do Saara! 💧"
+                ];
+                const message = messages[Math.floor(Math.random() * messages.length)];
+    
+                const audioFiles = [
+                    "drop-01.mp3",
+                    "drop-02.mp3",
+                    "drop-03.mp3",
+                    "drop-04.mp3"
+                ];
+    
+                const audioFile = audioFiles[Math.floor(Math.random() * audioFiles.length)];
+    
+                chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                    chrome.scripting.executeScript({
+                        target: { tabId: tabs[0].id },
+                        func: (pos, msg, audio, playSound, volume) => {
+                            const reminderDiv = document.createElement('div');
+                            reminderDiv.textContent = msg;
+                            reminderDiv.style.position = "fixed";
+                            reminderDiv.style.backgroundColor = "rgba(0, 123, 255, 0.9)";
+                            reminderDiv.style.color = "white";
+                            reminderDiv.style.padding = "15px 25px";
+                            reminderDiv.style.borderRadius = "10px";
+                            reminderDiv.style.fontSize = "20px";
+                            reminderDiv.style.fontWeight = "bold";
+                            reminderDiv.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)";
+                            reminderDiv.style.zIndex = "9999";
+                            reminderDiv.style.display = "flex";
+                            reminderDiv.style.justifyContent = "center";
+                            reminderDiv.style.alignItems = "center";
+                            reminderDiv.style.transition = "opacity 0.5s";
+    
+                            const closeButton = document.createElement('button');
+                            closeButton.textContent = "X";
+                            closeButton.style.marginLeft = "10px";
+                            closeButton.style.backgroundColor = "red";
+                            closeButton.style.color = "white";
+                            closeButton.style.border = "none";
+                            closeButton.style.borderRadius = "50%";
+                            closeButton.style.width = "25px";
+                            closeButton.style.height = "25px";
+                            closeButton.style.cursor = "pointer";
+                            closeButton.style.fontSize = "16px";
+                            closeButton.addEventListener('click', () => reminderDiv.remove());
+                            reminderDiv.appendChild(closeButton);
+    
+                            switch (pos) {
+                                case "top-left":
+                                    reminderDiv.style.top = "20px";
+                                    reminderDiv.style.left = "20px";
+                                    break;
+                                case "top-right":
+                                    reminderDiv.style.top = "20px";
+                                    reminderDiv.style.right = "20px";
+                                    break;
+                                case "bottom-left":
+                                    reminderDiv.style.bottom = "20px";
+                                    reminderDiv.style.left = "20px";
+                                    break;
+                                case "bottom-right":
+                                    reminderDiv.style.bottom = "20px";
+                                    reminderDiv.style.right = "20px";
+                                    break;
+                                case "center":
+                                    reminderDiv.style.top = "50%";
+                                    reminderDiv.style.left = "50%";
+                                    reminderDiv.style.transform = "translate(-50%, -50%)";
+                                    break;
+                            }
+    
+                            if (playSound) {
+                                const audioElement = new Audio(audio);
+                                audioElement.volume = volume / 100;
+                                audioElement.play();
+                            }
+    
+                            document.body.appendChild(reminderDiv);
+                            reminderDiv.style.opacity = "1";
+                            setTimeout(() => {
+                                reminderDiv.style.opacity = "0";
+                                setTimeout(() => { reminderDiv.remove(); }, 500);
+                            }, 40000);
+                        },
+                        args: [position, message, chrome.runtime.getURL(audioFile), data.playSound, data.volume]
+                    });
                 });
-            });
-        }
-    })
+            }
+        });
+    }
 });
--- popup.js
+++ popup.js
@@ -7,25 +7,36 @@ document.addEventListener("DOMContentLoaded", () => {
     const positionSelect = document.getElementById("position");
     const toggleSound = document.getElementById("toggle-sound");
     const volumeSlider = document.getElementById("volume");
-    const volumeValue = document.getElementById('volume-value');
+    const volumeValue = document.getElementById("volume-value");
+    const toggleReminderTimer = document.getElementById("toggle-reminder-timer");
+    const selects = document.querySelectorAll("select");
 
     // Ver se API do Chrome está disponível
     if (chrome.storage) {
         // Carregar o que foi salvo
-        chrome.storage.sync.get(["reminderInterval", "reminderActive", "reminderPosition", "playSound", "volume"], (result) => {
+        chrome.storage.sync.get(["reminderInterval", "reminderActive", "reminderPosition", "playSound", "volume", "disableDuration"], (result) => {
             if (result.reminderInterval) {
                 intervalSelect.value = result.reminderInterval;
             }
             toggleReminder.checked = !!result.reminderActive;
-            reminderStatus.textContent = result.reminderActive ? "Lembretes ativados" : "Lembretes desativados";
             positionSelect.disabled = !result.reminderActive;
+            toggleReminderTimer.disabled = !!result.reminderActive;
+            reminderStatus.textContent = result.reminderActive ? "Lembretes ativados" : "Lembretes desativados";
+
             if (result.reminderPosition) {
                 positionSelect.value = result.reminderPosition;
             }
+
             toggleSound.checked = !!result.playSound;
             volumeSlider.value = result.volume || 100;
             volumeValue.textContent = `${volumeSlider.value}%`;
             volumeSlider.disabled = !result.playSound;
+
+            if (result.disableDuration) {
+                toggleReminderTimer.value = result.disableDuration;
+            }
+
+            updateReminderStatus();
         });
 
         // Salvar configs
@@ -35,38 +46,79 @@ document.addEventListener("DOMContentLoaded", () => {
             const selectedPosition = positionSelect.value;
             const selectedSound = toggleSound.checked;
             const selectedVolume = volumeSlider.value;
+            const disableDuration = toggleReminderTimer.value;
+
             chrome.storage.sync.set({
                 reminderInterval: selectedInterval,
                 reminderActive: selectedReminder,
                 reminderPosition: selectedPosition,
                 playSound: selectedSound,
-                volume: selectedVolume
+                volume: selectedVolume,
+                disableDuration: disableDuration
             }, () => {
                 statusText.textContent = "Configuração salva!";
-                chrome.runtime.sendMessage({ action: "setAlarm", interval: selectedInterval, reminder: selectedReminder, position: selectedPosition });
+                chrome.runtime.sendMessage({
+                    action: "setAlarm",
+                    interval: selectedInterval,
+                    reminder: selectedReminder,
+                    position: selectedPosition,
+                    disableDuration: disableDuration
+                });
             });
         });
     } else {
         console.error("A API chrome.storage não está disponível. Execute como extensão.");
     }
 
+    // Carregar selects ao iniciar
+    selects.forEach(select => {
+        // Verificar se o valor está salvo no chrome.storage
+        chrome.storage.sync.get(select.id, (result) => {
+            const savedValue = result[select.id];
+            if (savedValue) {
+                select.value = savedValue;
+            }
+        });
+    
+        // Salvar última seleção
+        select.addEventListener("change", () => {
+            const valueToSave = select.value;
+            chrome.storage.sync.set({ [select.id]: valueToSave });
+        });
+    });
+
+    // Atualizar cor do status do lembrete
+    const updateReminderStatus = () => {
+        if (toggleReminder.checked) {
+            reminderStatus.textContent = "Lembretes ativados";
+            reminderStatus.style.color = "green";
+        } else {
+            reminderStatus.textContent = "Lembretes desativados";
+            reminderStatus.style.color = "red";
+        }
+    };
+
+    // Listener que atualiza status ao mudar checkbox '<input type="checkbox" id="toggle-reminder">'
+    toggleReminder.addEventListener("change", updateReminderStatus);
+
     // Altera Status dos lembretes (Ativado/Desativado)
     toggleReminder.addEventListener("change", () => {
         const isChecked = toggleReminder.checked;
         positionSelect.disabled = !isChecked;
         reminderStatus.textContent = isChecked ? "Lembretes ativados" : "Lembretes desativados";
+        toggleReminderTimer.disabled = !isChecked;
         chrome.storage.sync.set({ reminderActive: isChecked });
-    })
+    });
 
     // Altera Status do som (Ativado/Desativado)
     toggleSound.addEventListener("change", () => {
         const isSoundChecked = toggleSound.checked;
         volumeSlider.disabled = !isSoundChecked;
         chrome.storage.sync.set({ playSound: isSoundChecked });
-    })
+    });
 
     // Atualiza o valor do volume
     volumeSlider.addEventListener("input", () => {
         volumeValue.textContent = `${volumeSlider.value}%`;
-    })
+    });
 });
